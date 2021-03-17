59dddc6de0dba367e6e94712ff449c197cdbe639
mainelli <57752759+mainelli@users.noreply.github.com>
2021-03-17 21:19:46 +0100

    testing again

---
+++ cmake-build-debug/CMakeFiles/my_1006_hw2.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_1006_hw2.dir/main.cpp.o"
+  "CMakeFiles/my_1006_hw2.dir/matrix_algo.cpp.o"
+  "CMakeFiles/my_1006_hw2.dir/triangulation.cpp.o"
+  "CMakeFiles/my_1006_hw2.dir/triangulation_method.cpp.o"
+  "my_1006_hw2"
+  "my_1006_hw2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/my_1006_hw2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/my_1006_hw2.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for my_1006_hw2.
+# This may be replaced when dependencies are built.
